5db9156c4a64c76ce11d988dfe2f99b6b1a5ee64
Shashankv1308 <shashushashank407@gmail.com>
2026-01-24 23:51:22 +0530

    JwtAuthenticationFilter and login

---
+++ src/main/java/com/spring/ecommerce/auth/dto/AuthResponse.java
@@ -0,0 +1,11 @@
+package com.spring.ecommerce.auth.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthResponse 
+{
+    private String token;
+}
--- src/main/java/com/spring/ecommerce/security/SecurityConfig.java
+++ src/main/java/com/spring/ecommerce/security/SecurityConfig.java
@@ -9,10 +9,19 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
 public class SecurityConfig 
 {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) 
+    {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() 
     {
@@ -37,6 +46,11 @@ public class SecurityConfig
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/auth/**").permitAll()
                 .anyRequest().authenticated()
+            );
+            
+        http.addFilterBefore(
+                jwtAuthenticationFilter,
+                UsernamePasswordAuthenticationFilter.class
             );
 
         return http.build();
